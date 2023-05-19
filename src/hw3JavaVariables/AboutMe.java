87550b30917192cb215ba08ae91a1530febda05d
Shailamukta <shailakhanum7@gmail.com>
2023-05-18 22:34:19 -0400

    homework

---
+++ src/hw3JavaVariables/AboutMe.java
@@ -0,0 +1,24 @@
+package hw3JavaVariables;
+
+public class AboutMe {
+
+	public String name = "Shaila Khanum Mukta"; // variable initialized
+	public byte age = 33;
+	public short monthlysalary = 30000;
+	public int yearlysalary; // variable declared
+	public long passportnumber = 377789999l;
+	public float height = 5.00f;
+	public double cgpa = 4.75;
+	public char asian = 'Y';
+	public boolean isjavafun = true;
+
+	public static void main(String[] args) {
+
+		AboutMe aboutMe = new AboutMe();
+
+		System.out.println("My name : " + aboutMe.name + "\nMy age: " + aboutMe.age + "\nMy height: " + aboutMe.height
+				+ "\nAsian : " + aboutMe.asian + " , \nYearly salary: " + aboutMe.yearlysalary);
+
+	}
+
+}
